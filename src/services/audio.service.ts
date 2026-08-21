a347177e42fd9b0613741386e73e9a6f5fad3643
Sannisa12 <Sannisa.megarestya@gmail.com>
2026-08-21 13:33:15 +0700

    check if html5 false.

--- src/services/audio.service.ts
+++ src/services/audio.service.ts
@@ -43,7 +43,7 @@ export class AudioService {
             src: sound.src,
             volume: options.volume,
             loop: options.loop,
-            html5: true,
+            html5: false,
 
             onloaderror(id: number, err: unknown) {
                 console.error("Load Error:", id, err);
